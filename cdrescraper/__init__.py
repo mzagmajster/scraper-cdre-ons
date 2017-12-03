c00287b74f5c16e467a2709a675bacd309a4ecf1
Matic Zagmajster <zagm101@gmail.com>
2017-12-03 03:00:18 +0100

    Add auto. folder creation
    cdrescraper: To prevent files from being overwritten because they have the same name we enabled moving files to separate directories on first level (level 0).

--- cdrescraper/__init__.py
+++ cdrescraper/__init__.py
@@ -1,20 +1,26 @@
-from os import environ
+from os import environ, listdir, makedirs
+from os.path import isfile, abspath, dirname, exists
+from shutil import move
 
 from minsplinter import MinSplinter
 
 
 ROOT_DIRS = [
-	"https://cdre.ons.org.br/CDRE%20%20Processo%20ACOMPH%20%20Acompanhamento%20Hidrolgico/Forms/AllItems.aspx",
-	"https://cdre.ons.org.br/CDRE%20%20Processo%20Relatrio%20Dirio%20da%20Situao%20HidrulicoH/Forms/AllItems.aspx"
+	"https://cdre.ons.org.br/CDRE%20%20Processo%20Relatrio%20Dirio%20da%20Situao%20HidrulicoH/Forms/AllItems.aspx",
+	"https://cdre.ons.org.br/CDRE%20%20Processo%20ACOMPH%20%20Acompanhamento%20Hidrolgico/Forms/AllItems.aspx"
 
 ]
 
 def get_config():
+	instance_path = abspath(dirname(__file__)) + '/../instance'
+	instance_path = instance_path.replace('\\', '/')
+
 	c = {
 		'SCDRE_USR': environ['SCDRE_USR'],
 		'SCDRE_PWD': environ['SCDRE_PWD'],
 		'SCDRE_URL': environ['SCDRE_URL'],
-		'SCDRE_FIREFOX_PROFILE': environ['SCDRE_FIREFOX_PROFILE']
+		'SCDRE_FIREFOX_PROFILE': environ['SCDRE_FIREFOX_PROFILE'],
+		'SCDRE_INSTANCE_PATH': instance_path,
 	}
 	return c
 
@@ -27,9 +33,9 @@ class FileDownloader(object):
 			'PROFILE': self.config['SCDRE_FIREFOX_PROFILE']
 		}
 		self.spobj = MinSplinter(spobj_conf)
-		self.fdobj = MinSplinter(spobj_conf)
 		self._file = ''
 		self._count = 0
+		self._relevant_dir = ''
 
 	def login(self):
 		self.spobj.visit(self.config['SCDRE_URL'])
@@ -63,20 +69,20 @@ class FileDownloader(object):
 
 		return False
 
-
-	def list(self, link):
+	def list(self, link, level):
 		self.spobj.visit(link)
 		self.spobj.wait()
 
 		# Initial selector.
 		s = '#onetidDoclibViewTbl0 > tbody > tr'
 		el = self.spobj.find_elements('css', s)
-		print('el ', len(el))
+		# print('el ', len(el))
 
 		i = 0
 		h = ''
 		text = ''
 		dirs = list()
+		dir_names = list()
 
 		# Get file link.
 		s2 = 'td:nth-child(3) a'
@@ -94,13 +100,40 @@ class FileDownloader(object):
 					self._count += 1
 				else:
 					dirs.append(h)
+					dir_names.append(text)
+					
 			# print('is_file ', self.is_file(h), ' --> ', text, ' --> ', h)
 			i += 1
 
-		# print('dirs ', dirs)
+		# print('dirs ', dir_names)
+		# We need to create directories on first lavel for proper file grouping.
+		if not level and len(dir_names):
+			p = self.config['SCDRE_INSTANCE_PATH'] + '/' + dir_names[0]
+			
+			if not exists(p):
+				makedirs(p)
+
+			self._relevant_dir = dir_names[0]
+
 		# Iterate over other directories.
-		for d in dirs:
-			self.list(d)
+		j = 0
+		while j < len(dirs):
+			level += 1
+			self.list(dirs[j], level)
+			j += 1 
+
+	def move_files(self):
+		# Forbidden files.
+		f = ['__init__']
+		d = self.config['SCDRE_INSTANCE_PATH'] + '/' + self._relevant_dir
+		for file in listdir(self.config['SCDRE_INSTANCE_PATH']):
+			# File must not be moved or already exists.
+			if file in f or exists('/'.join([d, file])):
+				continue
+
+			p = self.config['SCDRE_INSTANCE_PATH'] + '/' + file
+			if isfile(p):
+				move(p, d)
 
 	def statistics():
 		print(self._count, ' files downloaded.')
--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from time import sleep
 import click
 
 from cdrescraper import get_config, FileDownloader, ROOT_DIRS
@@ -14,7 +15,11 @@ def download_files():
 	o = FileDownloader(conf)
 	o.login()
 	for  d in ROOT_DIRS:
-		o.list(d)
+		o.list(d, 0)
+		# Wait for downloading process to complete.
+		print('Waiting for downloads to complete.')
+		sleep(7)
+		self.move_files()
 
 	o.statistics()
 
@@ -25,8 +30,21 @@ def check_state():
 	pass
 
 
+@click.command('test')
+def test():
+	conf = get_config()
+	print(conf)
+	"""o = FileDownloader(conf)
+	o.login()
+	d = 'https://cdre.ons.org.br/CDRE%20%20Processo%20Relatrio%20Dirio%20da%20Situao%20HidrulicoH/Forms/AllItems.aspx?RootFolder=%2FCDRE%20%20Processo%20Relatrio%20Dirio%20da%20Situao%20HidrulicoH%2FRDH%5F2014%2F01%5FJaneiro&FolderCTID=0x0120000A691184EC3EB3468DDF9C6B199E5B58&View=%7BDCD6C8DB%2DC1DB%2D4BF7%2DB375%2D39FA66E4AB70%7D'
+	o.list(d, 0)
+	o.statistics()"""
+
+
+
 cli.add_command(download_files)
 cli.add_command(check_state)
+cli.add_command(test)
 
 if __name__ == '__main__':
 	cli()
